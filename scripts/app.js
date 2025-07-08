9ba9e58aec729a51f9612cdb23744f68b0719d95
DANIYA250496 <daniyamanickal1996@gmail.com>
2025-07-07 20:01:35 -0400

    week10lab

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+const app=Vue.createApp({
+    data(){
+        return {
+            title:'Week 10 Lab',
+            firstName:'',
+            lastName:'',
+            quantity: 0,
+            pricePerItem: 10
+        };
+    },
+    computed:{
+        fullName(){
+            return this.firstName+' '+this.lastName;
+        },
+        totalPrice(){
+            return this.quantity*this.pricePerItem;
+        }
+    },
+    methods:{
+        clearField(){
+            this.firstName='';
+            this.lastName='';
+            this.quantity= 0;
+        }
+    }
+});
+app.mount('#app');
